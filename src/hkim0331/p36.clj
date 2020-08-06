9f971f6364ff699185d2d86d87d74de15d59d82f
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2020-08-07 05:59:58 +0900

    commit from mbp2

--- src/hkim0331/p36.clj
+++ src/hkim0331/p36.clj
@@ -1,4 +1,5 @@
-(ns hkim0331.p36)
+(ns hkim0331.p36
+  (:require [hkim0331.p4 :refer [palindrome-number?]]))
 
 ; The decimal number, 585 = 1001001001(2) , is palindromic
 ; in both bases.
@@ -8,3 +9,30 @@
 
 ; (Please note that the palindromic number, in either base, may)
 ; not include leading zeros.
+
+; 先頭にゼロを許さないので、奇数に限る。
+
+(def pal-10 (filter palindrome-number? (range 1 1000000 2)))
+
+(defn base-aux [n num ret]
+  (if (zero? num)
+    (reverse ret)
+    (base-aux n (quot num n) (conj ret (rem num n)))))
+
+(defn base [n num]
+  (if (zero? num)
+    [0]
+    (base-aux n num [])))
+
+;(base 2 16)
+;(base 2 999999)
+
+(defn pal-n [n num]
+ (let [bn (base n num)]
+  (= bn (reverse bn))))
+
+;(pal-n 2 585)
+
+; (time (reduce + (filter #(pal-n 2 %) pal-10)))
+; "Elapsed time: 510.9467 msecs"
+; 872187
