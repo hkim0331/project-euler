90647b1e48779c725756cff3063248b23da25b4d
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2020-08-22 10:24:58 +0900

    p62.clj

--- src/hkim0331/p62.clj
+++ src/hkim0331/p62.clj
@@ -7,25 +7,38 @@
 (defn char> [c1 c2]
   (> (int c1) (int c2)))
 
+; first version.
+;
+; not returning key of the hash,
+; must return lowest integer which produces the hash key(cubic number).
+;
+; (defn p62' [n h]
+;   (loop [i 300 hsh h]
+;     (let [cube (* i i i)
+;           digits (apply str (sort char> (seq (str cube))))
+;           maybe-count (hsh digits)]
+;       (cond
+;       		(nil? maybe-count)	(recur (inc i) (assoc hsh digits 1))
+;         (= maybe-count (- n 1)) [i cube]
+;       		:else	(recur (inc i) (update hsh digits inc))))))
 
-(defn p62 [n h]
-  (loop [i 300 hsh h]
-;    (prn i hsh)
+; (p62' 3 {})
+;; [405 66430125]
+;; correct, but must return 41063625, first cube of the set.
+
+
+(defn p62 [n hsh]
+  (loop [i 300 h hsh]
     (let [cube (* i i i)
-          digits (apply str (sort char> (seq (str cube))))
-          maybe-count (hsh digits)]
-;      (prn cube maybe-count digits)
-      (cond
-      		(nil? maybe-count)
-      		(recur (inc i) (assoc hsh digits 1))
+          digits (apply str (sort char> (seq (str cube))))]
+      (if (= (- n 1) (count (h digits)))
+        (conj (h digits) cube)
+        (recur (inc i) (update h digits #(conj % cube)))))))
 
-      		(< maybe-count n)
-      		(recur (inc i) (update hsh digits inc))
+;(p62 3 {})
+; (66430125 56623104 41063625)
+; correct.
 
-      		:else [cube, hsh]))))
-
-
-; (p62 3)
-; 8120601000
-; 41063625
-; 違う。(
+; (time (p62 5 {}))
+; "Elapsed time: 27.587507 msecs"
+; (589323567104 569310543872 373559126408 352045367981 127035954683))
